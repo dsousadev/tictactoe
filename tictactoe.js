099065bdea0141282d7d4dc82e5ed0b1344c13ba
David de Sousa <admin@anatta.local>
2017-06-12 16:58:37 -0700

    tic tac toe

---
+++ tictactoe.js
@@ -0,0 +1,105 @@
+ var prompt = require('prompt');
+ 
+  // 
+  // Start the prompt 
+  // 
+  prompt.start();
+ 
+  // 
+  // Get two properties from the user: username and email 
+  // 
+
+//   console.log(`   1 2 3
+// 1 | | | |
+// 2 | | | |
+// 3 | | | |`);
+var row1 = ["_","_","_"];
+var row2 = ["_","_","_"];
+var row3 = ["_","_","_"];
+var matrix = [["_","_","_"], ["_","_","_"], ["_","_","_"]]
+var moveCount = 1;
+
+console.log(" ",['1','2','3']);
+console.log("1", matrix[0]);
+console.log("2", matrix[1]);
+console.log("3", matrix[2]);
+
+function getMove() {
+  prompt.get(['row', 'column'], function (err, result) {
+    var piece;
+    if (moveCount % 2 === 0) {
+      piece = 'X';
+    } else {
+      piece = "O";
+    }
+    if(matrix[result.row - 1][result.column - 1] === '_') {
+        matrix[result.row - 1][result.column - 1] = piece;
+        console.log(" ",['1','2','3']);
+        console.log("1", matrix[0]);
+        console.log("2", matrix[1]);
+        console.log("3", matrix[2]);
+        moveCount++;
+        if(checkBoard(matrix)) {
+            console.log("You Win!");
+        } else if (moveCount < 10) {
+            getMove();
+        } else {
+            console.log("DRAW!")
+        }
+    } else {
+      console.log("Sorry that place is already taken, please try again.");
+      getMove();
+      
+    }
+    
+
+  });
+}
+
+function checkBoard (matrix) {
+    var row1 = matrix[0];
+    var row2 = matrix[1];
+    var row3 = matrix[2];
+    var col1 = [row1[0], row2[0], row3[0]];
+    var col2 = [row1[1], row2[1], row3[1]];
+    var col3 = [row1[2], row2[2], row3[2]];
+
+
+     if (row1[0] === "X" && row1[1] === "X" && row1[2] === "X") {
+        return "You Win!"
+     } else if (row2[0] === "X" && row2[1] === "X" && row2[2] === "X") {
+        return "You Win!" 
+     } else if (row3[0] === "X" && row3[1] === "X" && row3[2] === "X") {
+        return "You Win!" 
+     } else if (col1[0] === "X" && col1[1] === "X" && col1[2] === "X") {
+        return "You Win!" 
+     } else if (col2[0] === "X" && col2[1] === "X" && col2[2] === "X") {
+        return "You Win!" 
+     } else if (col3[0] === "X" && col3[1] === "X" && col3[2] === "X") {
+        return "You Win!" 
+     } else if (row1[0] === "O" && row1[0] === "O" && row1[2] === "O") {
+        return "You Win!" 
+     } else if (row2[0] === "O" && row2[0] === "O" && row2[2] === "O") {
+        return "You Win!" 
+     } else if (row3[0] === "O" && row3[0] === "O" && row3[2] === "O") {
+        return "You Win!" 
+     } else if (col1[0] === "O" && col1[1] === "O" && col1[2] === "O") {
+        return "You Win!" 
+     } else if (col2[0] === "O" && col2[1] === "O" && col2[2] === "O") {
+        return "You Win!" 
+     } else if (col3[0] === "O" && col3[1] === "O" && col3[2] === "O") {
+        return "You Win!" 
+     } else if (row1[0] === "X" && row2[1] === "X" && row3[2] === "X") {
+        return "You Win!" 
+     } else if (row1[2] === "X" && row2[1] === "X" && row3[0] === "X") {
+        return "You Win!" 
+     } else if (row1[0] === "O" && row2[1] === "O" && row3[2] === "O") {
+        return "You Win!" 
+     } else if (row1[2] === "O" && row2[1] === "O" && row3[0] === "O") {
+        return "You Win!" 
+     }
+}
+
+getMove();
+
+
